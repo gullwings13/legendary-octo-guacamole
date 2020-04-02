afad1451877364c4b1b6d3fa487732f04cc4b56a
DARK\Trev <trevor.smith.holbourn@gmail.com>
2020-04-01 21:56:16 -0400

    added move systems

---
+++ Assets/Scripts/CubeMoveSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class CubeMoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities.WithName("CubeMoveSystem").ForEach((ref Translation position, ref Rotation rotation, ref CubeData cubeData) =>
+        {
+            position.Value -= 0.1f * math.up();
+            if (position.Value.y <0)
+            {
+                position.Value.y = 80;
+            }
+        }).Schedule(inputDeps);
+        
+        return jobHandle;
+    }
+}
---
+++ Assets/Scripts/ECSManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine.Rendering;
+
+public class ECSManager : MonoBehaviour
+{
+    private EntityManager manager;
+    public GameObject sheepPrefab;
+    private const int numSheep = 105000;
+    private const float range = 40;
+    private void Start()
+    {
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(sheepPrefab, settings);
+
+        for (int i = 0; i < numSheep; i++)
+        {
+            var instance = manager.Instantiate(prefab);
+            var position = transform.TransformPoint(new float3(UnityEngine.Random.Range(-range, range), UnityEngine.Random.Range(0, range*2), UnityEngine.Random.Range(-range, range)));
+            manager.SetComponentData(instance, new Translation {Value = position});
+            manager.SetComponentData(instance, new Rotation {Value = new quaternion(0,0,0,0)});
+        }
+    }
+}
