af91efc7730ab41073f8b07c8b2cfb737c496ce4
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-04-07 22:15:03 -0400

    Cleaned up some code and added scene for build

--- Assets/Scripts/MoveSystem.cs
+++ Assets/Scripts/MoveSystem.cs
@@ -11,36 +11,21 @@ public class MoveSystem : JobComponentSystem
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         float deltaTime = Time.DeltaTime;
-        float speed = 750f;
-        
-        float3 targetLocation = new float3(0,0,0);
-        
-        var jobHandle = Entities.
-            WithName("MoveSystem").
-            ForEach((ref Translation position, ref Rotation rotation, ref AsteroidData asteroidData) =>
+        float3 targetLocation = new float3(0, 0, 0);
+
+        var jobHandle = Entities.WithName("MoveSystem").ForEach(
+            (ref Translation position, ref Rotation rotation, ref AsteroidData asteroidData) =>
             {
-                // float3 pivot = targetLocation;
+                float3 diff = position.Value - targetLocation;
 
-                // var rotationalSpeed = deltaTime * speed * 1 / (math.distance(position.Value, pivot)*6f);
+                float dist = math.distance(position.Value, targetLocation);
+                float3 direction = math.normalize(diff);
+                float3 movementVector = asteroidData.velocity + -direction * (10 * (1 * 100 / (dist * dist)));
+                asteroidData.velocity = movementVector;
 
-                
-
-                // position.Value = math.mul(quaternion.AxisAngle(
-                //     new float3(0, 1, 0),
-                //     (asteroidData.axisAngle / asteroidData.axisAngle),
-                //     rotationalSpeed), position.Value - pivot) + pivot;
-
-                    float3 diff = position.Value - targetLocation;
-
-                    float dist = math.distance(position.Value, targetLocation);
-                    float3 direction = math.normalize(diff);
-                    float3 movementVector = asteroidData.velocity +  -direction * ( 10 * (1 * 100 / (dist * dist)));
-                    asteroidData.velocity = movementVector;
-
-                    position.Value += asteroidData.velocity;
-
+                position.Value += asteroidData.velocity;
             }).Schedule(inputDeps);
-        
+
         return jobHandle;
     }
 }
